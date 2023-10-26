d529513caf4312fcb796b2a6658b41fc0599521c
limaismael8901 <limaismael8901@gmail.com>
2023-10-26 05:24:13 -0300

    Adicionar arquivos de projeto.

---
+++ MailSenderAPI/infra/Services/MailService.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace MailSenderAPI.infra.Services
+{
+    public class MailService
+    {
+        private string smtpAddress => "smtp.gmail.com";
+        private int portNumber => 587;
+        private string emailFromAddress => "limaismael8901@gmail.com";
+        private string password => "A1l2dija";
+
+        public void AddEmailsToMailmensager(MailMessage mailMessage, string[] emails)
+        {
+            foreach (var email in emails)
+            {
+                mailMessage.To.Add(email);
+            }
+        }
+
+        public void SendEmail(string[] emails, string subject, string body, bool isHtml = false)
+        {
+            using (MailMessage mailMessage = new MailMessage())
+            {
+                mailMessage.From = new MailAddress(emailFromAddress);
+                AddEmailsToMailmensager(mailMessage, emails);
+                mailMessage.Subject = subject;
+                mailMessage.Body = body;
+                mailMessage.IsBodyHtml = isHtml;
+                using(SmtpClient smtp = new SmtpClient(smtpAddress, portNumber))
+                {
+                    smtp.EnableSsl = true;
+                    smtp.UseDefaultCredentials = false;
+                    smtp.Credentials = new NetworkCredential(emailFromAddress, password);
+                    smtp.Send(mailMessage);
+                } 
+            }
+        }
+
+    }
+}
